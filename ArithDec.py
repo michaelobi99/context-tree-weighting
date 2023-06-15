ea7b675a27581a5cdd0ebb9587bf1a01fbe503d8
Michael <michaelobi54@gmail.com>
2023-06-15 01:46:54 +0100

    everything working again

---
+++ ArithDec.py
@@ -0,0 +1,75 @@
+from BitIO import *
+import math
+from CTW import CTW
+
+high, low, underflow, code = 0xffffffff, 0, 0, 0
+shift = 31
+
+def decodeBit(prob: float, input: bitFile):
+    global high, low, underflow, code
+    split = low + int(((high - low) * prob))
+    bit = (code >= split)
+    if bit:
+        low = split
+    else:
+        high = split
+    while True:
+        if high & 0x80000000 == low & 0x80000000:
+            pass
+        elif (not (high & 0x40000000)) and (low & 0x40000000):
+            code ^= (1 << 30)
+            high ^= (1 << 30)
+            low ^= (1 << 30)
+        else:
+            break
+        low &= ((1 << shift) - 1)
+        low <<= 1
+        high &= ((1 << shift) - 1)
+        high <<= 1
+        high |= 1
+        code &= ((1 << shift) - 1)
+        code <<= 1
+        code |= inputBit(input)
+
+    return 1 if bit else 0
+
+def initializeDecoder(input: bitFile):
+    global code
+    for i in range(32):
+        code <<= 1
+        code |= inputBit(input)
+
+def expandFile(inputName: str, outputName: str, depth: int):
+    input: bitFile = openInputBitFile(inputName)
+    output: bitFile = openOutputBitFile(outputName)
+    bytestream = []
+    fileSize = 0
+    # read original file size from compressed stream
+    c = input.file.read(1)
+    while c != b' ':
+        bytestream.append(int.from_bytes(c))
+        c = input.file.read(1)
+    counter = len(bytestream) - 1
+    for i in bytestream:
+        fileSize += i * (10 ** counter)
+        counter -= 1
+    fileSize *= 8
+    # ..............................................
+    global high, low, underflow
+    initializeDecoder(input)
+    ctxTree = CTW(depth)
+    counter = 1
+    while counter <= fileSize:
+        p0 = math.exp(ctxTree.getLogPx(0))
+        p1 = math.exp(ctxTree.getLogPx(1))
+        assert (abs(p0 + p1 - 1) < 1e-6)
+
+        bit = decodeBit(p0, input)
+        outputBit(output, bit)
+        # model
+        ctxTree.update(bit, reverse=False, temp=False)
+        # arithmetic coding
+        counter += 1
+    else:
+        closeInputBitFile(input)
+        closeOutputBitFile(output)
---
+++ CTW.py
@@ -0,0 +1,77 @@
+import math
+import numpy as np
+from collections import defaultdict
+
+default = lambda: 2.0
+ktEstimate = defaultdict(default)
+
+
+def ktEstimator(count, bit: int):
+    s = sum(count)
+    if ktEstimate[s] == 2.0:
+        ktEstimate[s] = math.log(count[bit] + 0.5) - math.log(s + 1.0)
+    return ktEstimate[s]
+
+
+class Node():
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.child = [None] * 2
+        self.depth = parent.depth + 1 if parent != None else 0
+        self.count = [0] * 2
+        self.estimatedProb = 0.0
+        self.weightedProb = 0.0
+
+
+class CTW():
+    def __init__(self, contextDepth: int = 3, contextBits=None):
+        if contextBits == None:
+            contextBits = [0] * contextDepth
+        self.depth = contextDepth
+        self.root = Node()
+        self.contextBits = contextBits
+
+    def update(self, bit, reverse, temp):
+        logaddexp = lambda a, b: math.log(math.exp(a) + math.exp(b))
+        node = self.root
+        for i in range(self.depth, -1, -1):
+            # go one step deeper
+            if i != self.depth:
+                last: int = self.contextBits[i]
+                if node.child[last] is None:
+                    node.child[last] = Node(parent=node)
+                node = node.child[last]
+            # update node
+            if not reverse:
+                node.estimatedProb += math.log(node.count[bit] + 0.5) - math.log(sum(node.count) + 1.0)
+                # node.estimatedProb += ktEstimator(node.count, bit)
+                node.count[bit] += 1
+            else:
+                node.count[bit] -= 1
+                # node.estimatedProb -= ktEstimator(node.count, bit)
+                node.estimatedProb -= math.log(node.count[bit] + 0.5) - math.log(sum(node.count) + 1.0)
+
+        # node is leaf
+        assert node.depth == self.depth
+        node.weightedProb = node.estimatedProb
+
+        # back-propagate
+        while node.parent != None:
+            node = node.parent
+            assert node.depth < self.depth
+            pw0 = node.child[0].weightedProb if node.child[0] != None else 0.0
+            pw1 = node.child[1].weightedProb if node.child[1] != None else 0.0
+            node.weightedProb = math.log(0.5) + np.logaddexp(node.estimatedProb, pw0 + pw1)
+
+        if not temp:
+            self.contextBits = self.contextBits[1:]
+            self.contextBits.append(bit)
+
+    def getLogPx(self, bit):
+        pw = self.root.weightedProb
+        # dummy update
+        self.update(bit, reverse=False, temp=True)
+        pwx = self.root.weightedProb
+        # restore
+        self.update(bit, reverse=True, temp=True)
+        return pwx - pw
